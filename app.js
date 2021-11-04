f5edd39170d2727053d4eb37124916834ad98cc2
chrisclarkedev <cclarke8904@gmail.com>
2021-11-04 17:09:44 -0400

    arrow function for button, inserting to the DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+const sounds = ['coin', 'hadouken', 'sf2_theme'];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  btn.innerText = sound;
+
+  document.getElementById('buttons').appendChild(btn);
+});
